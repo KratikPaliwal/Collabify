c69c30b8112490a4b018b8a8098000d3a4df1f96
Kratik Paliwal <kratikpaliwal19@gmail.com>
2025-06-01 01:47:37 +0530

    Add header

---
+++ frontEnd/collabify/Components/Header.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+import { FaUser } from "react-icons/fa";
+import { FaBell } from "react-icons/fa";
+
+function Header() {
+  return (
+    <div className="h-20 w-full bg-white shadow-md flex items-center justify-between px-6 left-0 top-0 fixed">
+      <div className="flex gap-6">
+        <h1 className="text-xl font-bold text-blue-600">Collabify</h1>
+        <input
+          type="text"
+          placeholder="Search"
+          className="bg-blue-100 text-sm rounded-md px-3 py-1 w-64 border border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-300 text-black"
+        />
+      </div>
+      <div className="flex gap-6 text-gray-700">
+        <p className="cursor-pointer hover:text-blue-500">Home</p>
+        <p className="cursor-pointer hover:text-blue-500">Projects</p>
+        <p className="cursor-pointer hover:text-blue-500">Matches</p>
+        <div className="relative inline-block">
+          <div className="hover:text-blue-500 hover:bg-gray-100 hover:scale-105 transition rounded-full p-2 relative bottom-0 ">
+            <FaBell className="text-[20px]" />
+          </div>
+          <span className="absolute top-1 right-1 bg-red-500 text-white text-[10px] px-[6px] py-[1px] rounded-full">
+            1
+          </span>
+        </div>
+
+        <div className="hover:text-blue-500 hover:bg-gray-100 hover:scale-105 transition ">
+          <FaUser className="text-[20px]" />
+        </div>
+        <div></div>
+      </div>
+    </div>
+  );
+}
+
+export default Header;
