7bd3b2e48347c33294523dcc180124911982b2e2
Kratik Paliwal <kratikpaliwal19@gmail.com>
2025-06-01 11:16:46 +0530

    Add Profile section

--- frontEnd/collabify/src/App.jsx
+++ frontEnd/collabify/src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import './App.css'
 import Header from '../Components/Header'
+import Profile from '../Components/Profile'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -8,6 +9,7 @@ function App() {
   return (
     <>
     <Header />
+    <Profile />
     </>
   )
 }
